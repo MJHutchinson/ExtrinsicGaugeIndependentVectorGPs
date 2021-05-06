51ae766f87b0a63d8642ccfc4c80acacf5117422
Michael Hutchinson <hutchinson.micahel.john@gmail.com>
2021-05-06 15:07:32 +0100

    Functional product manifolds

--- riemannianvectorgp/manifold/embedded_manifold.py
+++ riemannianvectorgp/manifold/embedded_manifold.py
@@ -3,8 +3,12 @@ from typing import NamedTuple, Tuple
 import jax.numpy as jnp
 from tensorflow_probability.python.internal.backend import jax as tf2jax
 import numpy as np
+from jax import jit
+import jax.numpy as jnp
+import jax.scipy as jsp
+from functools import partial
 
-from .manifold import AbstractRiemannianMainfold
+from .manifold import AbstractRiemannianMainfold, ProductManifold
 
 
 class AbstractEmbeddedRiemannianManifold(AbstractRiemannianMainfold):
@@ -76,6 +80,7 @@ class AbstractEmbeddedRiemannianManifold(AbstractRiemannianMainfold):
         """
         raise NotImplementedError()
 
+    @partial(jit, static_argnums=(0,))
     def project_to_m(self, X, Y):
         """Projects a set of Euclidean locations and vectors to the intrinsic
         coordinates and tangent spaces.
@@ -99,6 +104,7 @@ class AbstractEmbeddedRiemannianManifold(AbstractRiemannianMainfold):
             jnp.swapaxes(self.projection_matrix(M), -1, -2), Y
         )
 
+    @partial(jit, static_argnums=(0,))
     def project_to_e(self, M, V):
         """Projects a set of intrinsic coordinates and tangent vectors to
         euclidean space.
@@ -122,6 +128,27 @@ class AbstractEmbeddedRiemannianManifold(AbstractRiemannianMainfold):
             tf2jax.linalg.matvec(self.projection_matrix(M), V),
         )
 
+    @partial(jit, static_argnums=(0,))
+    def tanget_projection(self, M1, M2):
+        p1 = self.projection_matrix(M1)
+        p2 = self.projection_matrix(M2)
+        # return jnp.einsum("...iem,...jfn->...jinm", p1, p2)
+        return tf2jax.linalg.matmul(
+            jnp.swapaxes(p2[..., :, np.newaxis, :, :], -1, -2),
+            p1[..., np.newaxis, :, :, :],
+        )
+
+    @partial(jit, static_argnums=(0,))
+    def euclidean_projection_matrix(self, M):
+        proj_mat = self.projection_matrix(M)
+        return jnp.einsum("...em,...fn->...ef", proj_mat, proj_mat)
+
+    @partial(jit, static_argnums=(0,))
+    def flatten_to_manifold(self, X, Y):
+        pi = self.euclidean_projection_matrix(self.e_to_m(X))
+        return jnp.einsum("...ij,...j->...i", pi, Y)
+
+    @partial(jit, static_argnums=(0,))
     def rotate_points(self, M, V, R):
         """Rotate a set of intrinsic coordinates and tangent vectors by a rotation R
 
@@ -152,6 +179,7 @@ class AbstractEmbeddedRiemannianManifold(AbstractRiemannianMainfold):
         V_ = (vector_transport_map @ V[..., np.newaxis]).squeeze(-1)
         return M_, V_
 
+    @partial(jit, static_argnums=(0,))
     def project_to_3d(self, M, V):
         """Projects intrinsic coordinates and tangent vectors to 3D for visualisation
 
@@ -175,7 +203,86 @@ class AbstractEmbeddedRiemannianManifold(AbstractRiemannianMainfold):
         ).squeeze()
         return X, Y
 
-    # def __repr__(
-    #     self,
-    # ):
-    #     return self.__class__.__bases__[-1].__name__
+    def __mul__(self, other):
+        if isinstance(other, AbstractEmbeddedRiemannianManifold):
+            return EmbeddedProductManifold(self, other)
+        elif isinstance(other, EmbeddedProductManifold):
+            return EmbeddedProductManifold(
+                self, *other.sub_manifolds, num_eigenfunctions=other.num_eigenfunctions
+            )
+        else:
+            raise NotImplementedError()
+
+    def __rmul__(self, other):
+        if isinstance(other, AbstractEmbeddedRiemannianManifold):
+            return EmbeddedProductManifold(other, self)
+        elif isinstance(other, EmbeddedProductManifold):
+            return EmbeddedProductManifold(
+                *other.sub_manifolds, self, num_eigenfunctions=other.num_eigenfunctions
+            )
+        else:
+            raise NotImplementedError()
+
+
+class EmbeddedProductManifold(AbstractEmbeddedRiemannianManifold, ProductManifold):
+    def __init__(self, *sub_manifolds, **kwargs):
+        super().__init__(*sub_manifolds, **kwargs)
+        self.sub_embedded_dimensions = [
+            sm.embedded_dimension for sm in self.sub_manifolds
+        ]
+        self.embedded_dimension = sum(self.sub_embedded_dimensions)
+
+    @partial(jit, static_argnums=(0,))
+    def m_to_e(self, M):
+        sub_M = jnp.split(M, self.sub_dimensions[:-1], axis=-1)
+        sub_X = [
+            self.sub_manifolds[i].m_to_e(sub_M[i])
+            for i in range(len(self.sub_manifolds))
+        ]
+        return jnp.concatenate(sub_X, axis=-1)
+
+    @partial(jit, static_argnums=(0,))
+    def e_to_m(self, E):
+        sub_E = jnp.split(E, self.sub_dimensions[:-1], axis=-1)
+        sub_M = [
+            self.sub_manifolds[i].e_to_m(sub_E[i])
+            for i in range(len(self.sub_manifolds))
+        ]
+        return jnp.concatenate(sub_M, axis=-1)
+
+    @partial(jit, static_argnums=(0,))
+    def projection_matrix(self, M):
+        sub_M = jnp.split(M, self.sub_dimensions[:-1], axis=-1)
+        sub_projection_matricies = [
+            self.sub_manifolds[i].projection_matrix(sub_M[i])
+            for i in range(len(self.sub_manifolds))
+        ]
+        return jax.vmap(jsp.linalg.block_diag)(*sub_projection_matricies)
+
+    def __mul__(self, other):
+        if isinstance(other, AbstractEmbeddedRiemannianManifold):
+            return EmbeddedProductManifold(*self.sub_manifolds, other)
+        elif isinstance(other, EmbeddedProductManifold):
+            return EmbeddedProductManifold(
+                *self.sub_manifolds,
+                *other.sub_manifolds,
+                num_eigenfunctions=max(
+                    self.num_eigenfunctions, other.num_eigenfunctions
+                )
+            )
+        else:
+            raise NotImplementedError()
+
+    def __rmul__(self, other):
+        if isinstance(other, AbstractEmbeddedRiemannianManifold):
+            return EmbeddedProductManifold(other, *self.sub_manifolds)
+        elif isinstance(other, EmbeddedProductManifold):
+            return EmbeddedProductManifold(
+                *other.sub_manifolds,
+                *self.sub_manifolds,
+                num_eigenfunctions=max(
+                    self.num_eigenfunctions, other.num_eigenfunctions
+                )
+            )
+        else:
+            raise NotImplementedError()
--- riemannianvectorgp/manifold/manifold.py
+++ riemannianvectorgp/manifold/manifold.py
@@ -1,6 +1,9 @@
 from abc import ABC, ABCMeta, abstractmethod
 import jax.numpy as jnp
 
+from jax import jit
+from functools import partial
+
 
 class AbstractRiemannianMainfold(ABC):
     dimension = None
@@ -24,3 +27,122 @@ class AbstractRiemannianMainfold(ABC):
         self,
     ):
         return self.__class__.__name__
+
+    def __mul__(self, other):
+        if isinstance(other, AbstractRiemannianMainfold):
+            return ProductManifold(self, other)
+        elif isinstance(other, ProductManifold):
+            return ProductManifold(
+                self, *other.sub_manifolds, num_eigenfunctions=other.num_eigenfunctions
+            )
+        else:
+            raise NotImplementedError()
+
+    def __rmul__(self, other):
+        if isinstance(other, AbstractRiemannianMainfold):
+            return ProductManifold(other, self)
+        elif isinstance(other, ProductManifold):
+            return ProductManifold(
+                *other.sub_manifolds, self, num_eigenfunctions=other.num_eigenfunctions
+            )
+        else:
+            raise NotImplementedError()
+
+
+class ProductManifold(AbstractRiemannianMainfold):
+    def __init__(self, *sub_manifolds, num_eigenfunctions=1000):
+        self.sub_manifolds = sub_manifolds
+        self.sub_dimensions = [sm.dimension for sm in self.sub_manifolds]
+        self.dimension = sum(self.sub_dimensions)
+        self.num_eigenfunctions = num_eigenfunctions
+
+        # precompute the eigenindicies of the product manifold eigenvalues
+        sub_manifold_eigenindicies = [
+            jnp.arange(self.num_eigenfunctions) for m in self.sub_manifolds
+        ]
+        sub_manifold_eigenvalues = [
+            m.laplacian_eigenvalue(i)
+            for m, i in zip(self.sub_manifolds, sub_manifold_eigenindicies)
+        ]
+        sub_manifold_eigenindicies = jnp.stack(
+            jnp.meshgrid(*sub_manifold_eigenindicies), axis=-1
+        ).reshape((-1, len(self.sub_manifolds)))
+        sub_manifold_eigenvalues = jnp.sum(
+            jnp.stack(jnp.meshgrid(*sub_manifold_eigenvalues), axis=-1), axis=-1
+        ).reshape(-1)
+        index = jnp.argsort(sub_manifold_eigenvalues)
+        self.sub_manifold_eigenindicies = sub_manifold_eigenindicies[index][
+            : self.num_eigenfunctions
+        ]
+
+    @partial(jit, static_argnums=(0))
+    def laplacian_eigenvalue(
+        self,
+        n: jnp.ndarray,
+    ) -> jnp.ndarray:
+        # TODO: SHARP EDGE! no check that the eigen index has been precomputed as it breaks jit'ing
+        # if jnp.any(n >= self.num_eigenfunctions):
+        #     raise ValueError(
+        #         f"Max n ({n.max()}) greater than the precomputed eigenvalues ({self.num_eigenfunctions}) for this product manifold"
+        #     )
+        return jnp.sum(
+            jnp.stack(
+                [
+                    m.laplacian_eigenvalue(self.sub_manifold_eigenindicies[n, i])
+                    for i, m in enumerate(self.sub_manifolds)
+                ],
+                axis=-1,
+            ),
+            axis=-1,
+        )
+
+    @partial(jit, static_argnums=(0))
+    def laplacian_eigenfunction(
+        self,
+        n: jnp.ndarray,
+        x: jnp.ndarray,
+    ) -> jnp.ndarray:
+        sub_x = jnp.split(x, self.sub_dimensions[:-1], axis=-1)
+        return jnp.prod(
+            jnp.stack(
+                [
+                    m.laplacian_eigenfunction(self.sub_manifold_eigenindicies[n, i], x)
+                    for i, (m, x) in enumerate(zip(self.sub_manifolds, sub_x))
+                ],
+                axis=-1,
+            ),
+            axis=-1,
+        )
+
+    def __repr__(
+        self,
+    ):
+        return "×".join([str(m) for m in self.sub_manifolds])
+
+    def __mul__(self, other):
+        if isinstance(other, AbstractRiemannianMainfold):
+            return ProductManifold(*self.sub_manifolds, other)
+        elif isinstance(other, ProductManifold):
+            return ProductManifold(
+                *self.sub_manifolds,
+                *other.sub_manifolds,
+                num_eigenfunctions=max(
+                    self.num_eigenfunctions, other.num_eigenfunctions
+                ),
+            )
+        else:
+            raise NotImplementedError()
+
+    def __rmul__(self, other):
+        if isinstance(other, AbstractRiemannianMainfold):
+            return ProductManifold(other, *self.sub_manifolds)
+        elif isinstance(other, ProductManifold):
+            return ProductManifold(
+                *other.sub_manifolds,
+                *self.sub_manifolds,
+                num_eigenfunctions=max(
+                    self.num_eigenfunctions, other.num_eigenfunctions
+                ),
+            )
+        else:
+            raise NotImplementedError()
