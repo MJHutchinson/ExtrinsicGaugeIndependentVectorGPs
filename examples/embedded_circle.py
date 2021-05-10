717a74d2c807bcc41ce6b7015ef183d2fb68131d
Michael Hutchinson <hutchinson.micahel.john@gmail.com>
2021-05-10 11:15:59 +0100

    Adds some dirty scripts that might be useful to others in dev

---
+++ examples/embedded_circle.py
@@ -0,0 +1,195 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+%load_ext autoreload
+%autoreload 2
+# %%
+# get_ipython().run_line_magic("load_ext", "autoreload")
+# get_ipython().run_line_magic("autoreload", "2")
+import numpy as np
+import jax
+import jax.numpy as jnp
+import jax.random as jr
+import jax.scipy as jsp
+from tensorflow_probability.python.internal.backend import jax as tf2jax
+
+import matplotlib.pyplot as plt
+from IPython.display import set_matplotlib_formats
+
+set_matplotlib_formats("svg")
+import sys
+
+from riemannianvectorgp.manifold import S1, EmbeddedS1, ProductManifold, EmbeddedProductManifold
+from riemannianvectorgp.kernel import ScaledKernel, ManifoldProjectionVectorKernel, MaternCompactRiemannianManifoldKernel, SquaredExponentialCompactRiemannianManifoldKernel
+from jax.config import config
+
+# config.update("jax_debug_nans", True)
+# config.update("jax_disable_jit", True)
+
+# %%
+class GlobalRNG:
+    def __init__(self, seed: int = np.random.randint(2147483647)):
+        self.key = jax.random.PRNGKey(seed)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        (ret_key, self.key) = jr.split(self.key)
+        return ret_key
+
+
+rng = GlobalRNG()
+# %%
+
+s1 = EmbeddedS1(0.5)
+
+m = jnp.linspace(0, jnp.pi*2, 26)[:-1, np.newaxis] % (2 * jnp.pi)
+v = jnp.ones_like(m)
+x, y = s1.project_to_e(m, v)
+
+# %%
+
+proj_mat = s1.projection_matrix(m)
+Pi = s1.euclidean_projection_matrix(m)
+# %%
+plt.plot(x[:,0], x[:, 1])
+plt.quiver(x[:,0], x[:, 1], proj_mat[:,0, 0], proj_mat[:, 1, 0])
+# plt.quiver(x[:,0], x[:, 1], Pi[:,1, 0], Pi[:, 1, 1])
+
+plt.gca().set_aspect('equal')
+# %%
+plt.plot(x[:,0], x[:, 1])
+plt.quiver(x[:,0], x[:, 1], Pi[:,0, 0], Pi[:, 0, 1])
+plt.quiver(x[:,0], x[:, 1], Pi[:,1, 0], Pi[:, 1, 1])
+
+plt.gca().set_aspect('equal')
+# %%
+plt.plot(x[:,0], x[:, 1])
+plt.quiver(x[:,0], x[:, 1], y[:,0], y[:, 1])
+plt.gca().set_aspect('equal')
+
+# %%
+y = jr.normal(next(rng), (x.shape))
+scale=10
+plt.plot(x[:,0], x[:, 1], zorder=1, color='black')
+plt.quiver(x[:,0], x[:, 1], y[:,0], y[:, 1], scale=scale, zorder=2, color='red')
+plt.gca().set_aspect('equal')
+
+proj_mat = s1.projection_matrix(m)
+v_ = jnp.einsum("...em,...e->...m", proj_mat, y)
+y_ = jnp.einsum("...em,...m->...e", proj_mat, v_)
+
+# eig_val, eig_vec = jnp.linalg.eig(Pi)
+
+# idx = eig_val.argsort()
+# eig_val = np.take_along_axis(eig_val, idx, axis=1)
+# eig_vec = np.take_along_axis(eig_vec, idx[..., np.newaxis], axis=1)
+
+# y_ = s1.flatten_to_manifold(x, y)
+plt.quiver(x[:,0], x[:, 1], y_[:,0], y_[:, 1], scale=scale, zorder=3, color='blue')
+# plt.quiver(x[:,0], x[:, 1], eig_vec[:,0,0], eig_vec[:,1,0], zorder=3, color='blue')
+# plt.quiver(x[:,0], x[:, 1], eig_vec[:,0,1], eig_vec[:,1,1], zorder=3, color='blue')
+
+m_, v_ = s1.project_to_m(x, y)
+_, y_ = s1.project_to_e(m_, v_)
+plt.quiver(x[:,0], x[:, 1], y_[:,0], y_[:, 1], scale=scale, zorder=3, color='orange')
+
+y_ = s1.flatten_to_manifold(x, y)
+plt.quiver(x[:,0], x[:, 1], y_[:,0], y_[:, 1], scale=scale, zorder=3, color='green')
+
+# y_ = jnp.einsum("...em,...fm,...f->...e", proj_mat, proj_mat, v_)
+# plt.quiver(x[:,0], x[:, 1], y_[:,0], y_[:, 1], scale=scale, zorder=3, color='purple')
+
+flatten_mat1 = tf2jax.linalg.matmul(proj_mat, jnp.swapaxes(proj_mat, -1, -2))
+y_ = tf2jax.linalg.matvec(flatten_mat1, y)
+plt.quiver(x[:,0], x[:, 1], y_[:,0], y_[:, 1], scale=scale, zorder=3, color='pink')
+
+flatten_mat2 = jnp.einsum("...em,...fn->...ef", proj_mat, proj_mat)
+y_ = jnp.einsum("...ef,...f->...e", flatten_mat2, y)
+plt.quiver(x[:,0], x[:, 1], y_[:,0], y_[:, 1], scale=scale, zorder=3, color='yellow')
+# %%
+
+tp = s1.tanget_projection(m, m)
+
+plt.plot(x[:,0], x[:,1])
+plt.plot(x[:,0]*(1 + tp[0, :, 0, 0]), x[:,1]*(1 + tp[0, :, 0, 0]))
+plt.gca().set_aspect('equal')
+plt.scatter(x[0,0], x[0,1])
+
+p1 = s1.projection_matrix(m)
+p2 = s1.projection_matrix(m)
+tp =  jnp.einsum("...iem,ef,...jfn->...jinm", p1, jnp.eye(2), p2)
+plt.plot(x[:,0]*(1 + tp[0, :, 0, 0]), x[:,1]*(1 + tp[0, :, 0, 0]))
+
+tp =  jnp.einsum("...iem,...jfn->...jinm", p1, p2)
+plt.plot(x[:,0]*(1 + tp[0, :, 0, 0]), x[:,1]*(1 + tp[0, :, 0, 0]))
+# %%
+
+m_, v_ = s1.project_to_m(x, y)
+
+print(jnp.mean(m - m_), jnp.mean(v - v_))
+# %%
+
+M = jnp.concatenate([m,m], axis=-1)
+jnp.split(M, [1], axis=-1)
+# %%
+
+s_2 = EmbeddedProductManifold(EmbeddedS1(0.5), EmbeddedS1(0.5))
+
+
+# %%
+m = jnp.linspace(0, jnp.pi*2, 30) % (2 * jnp.pi)
+m = jnp.meshgrid(m,m)
+m = jnp.stack([m_.flatten() for m_ in m], axis=-1)
+v = jnp.ones_like(m)
+
+x, y = s_2.project_to_e(m, v)
+plt.plot(x[:,0], x[:, 1], zorder=0)
+plt.quiver(x[:,0], x[:, 1], y[:,0], y[:, 1], zorder=1)
+plt.gca().set_aspect('equal')
+# %%
+s_2.projection_matrix(m).shape
+
+# %%
+
+s1 = S1(0.5)
+# kernel = ScaledKernel(SquaredExponentialCompactRiemannianManifoldKernel(s1, 100))
+kernel = ScaledKernel(MaternCompactRiemannianManifoldKernel(0.5,s1, 100))
+kernel_params = kernel.init_params(next(rng))
+sub_kernel_params = kernel_params.sub_kernel_params
+sub_kernel_params = sub_kernel_params._replace(log_length_scale=jnp.log(0.1))
+kernel_params = kernel_params._replace(sub_kernel_params=sub_kernel_params)
+kernel_params = kernel_params._replace(log_amplitude=-jnp.log(kernel.matrix(kernel_params, m,m)[0,0,0]))
+k = kernel.matrix(kernel_params, m, m)
+plt.plot(m[:, 0], k[0, :, 0, 0], label="EQ")
+
+# %%
+i = 20
+plt.plot(x[:,0], x[:,1])
+plt.scatter(x[i,0], x[i,1])
+plt.plot(x[:,0]*(1 + k[i, :, 0, 0]), x[:,1]*(1 + k[i, :, 0, 0]))
+plt.gca().set_aspect('equal')
+
+# %%
+
+s1 = EmbeddedS1(0.5)
+# kernel = ScaledKernel(SquaredExponentialCompactRiemannianManifoldKernel(s1, 100))
+kernel = ScaledKernel(ManifoldProjectionVectorKernel(MaternCompactRiemannianManifoldKernel(0.5,s1, 100), s1))
+kernel_params = kernel.init_params(next(rng))
+sub_kernel_params = kernel_params.sub_kernel_params
+sub_kernel_params = sub_kernel_params._replace(log_length_scale=jnp.log(0.1))
+kernel_params = kernel_params._replace(sub_kernel_params=sub_kernel_params)
+kernel_params = kernel_params._replace(log_amplitude=-jnp.log(kernel.matrix(kernel_params, m,m)[0,0,0,0]))
+k = kernel.matrix(kernel_params, m, m)
+plt.plot(m[:, 0], k[0, :, 0, 0], label="EQ")
+# %%
+plt.plot(x[:,0], x[:,1])
+plt.plot(x[:,0]*(1 + k[0, :, 0, 0]), x[:,1]*(1 + k[0, :, 0, 0]))
+plt.gca().set_aspect('equal')
+# %%
+_, k_ = s1.project_to_e(m, k[0, :, :, 0])
+plt.plot(x[:,0], x[:, 1], zorder=0)
+plt.quiver(x[:,0], x[:, 1], k_[:,0], k_[:, 1], zorder=1)
+plt.gca().set_aspect('equal')
+# %%
---
+++ tests/manual_test_kernel.py
@@ -0,0 +1,340 @@
+# %%
+%load_ext autoreload
+%autoreload 2
+import jax
+import numpy as np
+import jax.numpy as jnp
+import jax.random as jr
+import tensorflow_probability
+from tensorflow_probability.python.internal.backend import jax as tf2jax
+
+tfp = tensorflow_probability.experimental.substrates.jax
+tfk = tfp.math.psd_kernels
+
+import sys
+sys.path.insert(0, "..")
+from riemannianvectorgp.manifold import S1, EmbeddedS1, ProductManifold, EmbeddedProductManifold
+from riemannianvectorgp.kernel import (
+    ScaledKernel, 
+    ManifoldProjectionVectorKernel, 
+    MaternCompactRiemannianManifoldKernel, 
+    SquaredExponentialCompactRiemannianManifoldKernel,
+    FourierFeatures,
+    TFPKernel
+)
+
+from jax.config import config
+config.update("jax_enable_x64", True)
+# %%
+import matplotlib.pyplot as plt       
+
+class GlobalRNG:
+    def __init__(self, seed: int = np.random.randint(2147483647)):
+        self.key = jax.random.PRNGKey(seed)
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        (ret_key, self.key) = jr.split(self.key)
+        return ret_key
+
+rng = GlobalRNG()
+
+def eval_kernel(kernel, kernel_params, x, key, num_samples=1000, num_basis_functions=1000):
+    ff = FourierFeatures(kernel, num_basis_functions)
+    state = ff.init_state(kernel_params, num_samples, key)
+    f = ff(kernel_params, state, x)
+
+    k = kernel.matrix(kernel_params, x, x)
+    m_ff = jnp.mean(f,axis=0)
+    k_ff = jnp.mean(f[..., :, np.newaxis, :, np.newaxis] * f[..., np.newaxis, :, np.newaxis, :], axis=0)
+
+    m_err = - m_ff
+    m_mean_err = jnp.mean(m_err)
+    m_max_err = jnp.max(jnp.abs(m_err))
+
+    k_err = k - k_ff
+    k_mean_err = jnp.mean(k_err)
+    k_max_err = jnp.max(jnp.abs(k_err))
+
+    return m_mean_err, m_max_err, k_mean_err, k_max_err
+
+
+# %%
+def plot_kernel_approx(x,mu,K,mu_approx, K_approx, samples=False):    
+    fig = plt.figure()
+    # ax = fig.add_subplot(projection='polar')
+    ax = fig.add_subplot()
+
+    ax.plot(x,K_approx, zorder=2, color='blue', label='approx')
+    # ax.plot(x,mu_approx, zorder=2, color='blue')
+    # ax.fill_between(x, mu_approx+K_approx, mu_approx-K_approx, alpha=0.5, zorder=3, color='blue')
+
+    ax.plot(x,K,linewidth=2, zorder=4, color='orange', label='actual')
+    plt.legend()
+    # ax.plot(x,mu,linewidth=2, zorder=4, color='orange')
+    # ax.fill_between(x, mu+K, mu-K, alpha=0.5, zorder=3, color='orange')
+
+# %%
+kernel = ScaledKernel(TFPKernel(tfk.ExponentiatedQuadratic, 1, 1))
+kernel_params = kernel.init_params(next(rng))
+x = jnp.linspace(-5, 5, 101)[:, np.newaxis]
+num_samples = 20000
+num_basis_functions = 10000
+key = next(rng)
+
+ff = FourierFeatures(kernel, num_basis_functions)
+state = ff.init_state(kernel_params, num_samples, key)
+f = ff(kernel_params, state, x)
+
+k = kernel.matrix(kernel_params, x, x)
+m_ff = jnp.mean(f,axis=0)
+k_ff = jnp.mean(f[..., :, np.newaxis, :, np.newaxis] * f[..., np.newaxis, :, np.newaxis, :], axis=0)
+plot_kernel_approx(x[:,0], jnp.zeros_like(x[:,0]), k[0,:,0,0], m_ff[:,0], k_ff[0,:,0,0])
+# plt.title(f"Square exponential, samples: {num_samples}, basis functions: {num_basis_functions}")
+# %%
+i = 50
+plot_kernel_approx(x[:,0], jnp.zeros_like(x[:,0]), k[i,:,0,0], m_ff[:,0], k_ff[i,:,0,0])
+
+# %%
+
+k_err = k - k_ff
+mean_err = jnp.mean(k_err)
+max_err = jnp.max(jnp.abs(k_err))
+
+print(mean_err, max_err)
+# %%
+
+print(eval_kernel(kernel, kernel_params, x, next(rng)))
+
+# %%
+
+kernel = SquaredExponentialCompactRiemannianManifoldKernel(S1(0.5), truncation = 500)
+kernel_params = kernel.init_params(next(rng))
+kernel_params = kernel_params._replace(log_length_scale=jnp.log(0.1))
+x = jnp.linspace(0, 2*2*jnp.pi*kernel.manifold.radius, 101)[:, np.newaxis]
+num_samples = 20000
+num_basis_functions = 10000
+key = next(rng)
+
+ff = FourierFeatures(kernel, num_basis_functions)
+state = ff.init_state(kernel_params, num_samples, key)
+f = ff(kernel_params, state, x)
+
+
+k = kernel.matrix(kernel_params, x, x)
+m_ff = jnp.mean(f,axis=0)
+k_ff = jnp.mean(f[..., :, np.newaxis, :, np.newaxis] * f[..., np.newaxis, :, np.newaxis, :], axis=0)
+plot_kernel_approx(x[:,0], jnp.zeros_like(x[:,0]), k[0,:,0,0], m_ff[:,0], k_ff[0,:,0,0])
+plt.title(f"Circle Square exponential, samples: {num_samples}, basis functions: {num_basis_functions}")
+# %%
+nu=1.5
+kernel = MaternCompactRiemannianManifoldKernel(nu, S1(0.5), truncation = 500)
+kernel_params = kernel.init_params(next(rng))
+kernel_params = kernel_params._replace(log_length_scale=jnp.log(0.2))
+x = jnp.linspace(0, 2*2*jnp.pi*kernel.manifold.radius, 101)[:, np.newaxis]
+num_samples = 20000
+num_basis_functions = 10000
+key = next(rng)
+
+ff = FourierFeatures(kernel, num_basis_functions)
+state = ff.init_state(kernel_params, num_samples, key)
+f = ff(kernel_params, state, x)
+
+
+k = kernel.matrix(kernel_params, x, x)
+m_ff = jnp.mean(f,axis=0)
+k_ff = jnp.mean(f[..., :, np.newaxis, :, np.newaxis] * f[..., np.newaxis, :, np.newaxis, :], axis=0)
+plot_kernel_approx(x[:,0], jnp.zeros_like(x[:,0]), k[0,:,0,0], m_ff[:,0], k_ff[0,:,0])
+plt.title(f"Circle Matern {nu}, samples: {num_samples}, basis functions: {num_basis_functions}")
+
+# %%
+
+for nu in [0.5, 1.0, 1.5, 2.0, 2.5, 3.0]:
+    kernel = MaternCompactRiemannianManifoldKernel(nu, S1(0.5), truncation = 500)
+    kernel_params = kernel.init_params(next(rng))
+    kernel_params = kernel_params._replace(log_length_scale=jnp.log(0.3))
+    k = kernel.matrix(kernel_params, x, x)
+    plt.plot(x[:,0], k[0,:,0,0]/ k[0,0,0,0])
+    # plt.plot(x[:,0], k[0,:,0,0])
+
+# %%
+s1 = EmbeddedS1(0.5) # produce base manifold
+num_basis_functions = 1000
+num_samples = 2000
+man = EmbeddedProductManifold(s1,s1, num_eigenfunctions=num_basis_functions)
+
+n_points = 25
+m = np.linspace(0, jnp.pi*2, n_points+1)[:-1] % (2 * jnp.pi)
+m = jnp.meshgrid(m,m)
+m = jnp.stack([m_.flatten() for m_ in m], axis=-1)
+
+kernel = ScaledKernel(ManifoldProjectionVectorKernel(SquaredExponentialCompactRiemannianManifoldKernel(man, num_basis_functions), man))
+# kernel = ScaledKernel(MaternCompactRiemannianManifoldKernel(2.5,man, num_basis_functions))
+kernel_params = kernel.init_params(next(rng))
+sub_kernel_params = kernel_params.sub_kernel_params
+sub_kernel_params = sub_kernel_params._replace(log_length_scale=jnp.log(0.1))
+kernel_params = kernel_params._replace(sub_kernel_params=sub_kernel_params)
+kernel_params = kernel_params._replace(log_amplitude=-jnp.log(kernel.matrix(kernel_params, m,m)[0,0,0,0]))
+k = kernel.matrix(kernel_params, m, m)
+key = next(rng)
+
+ff = FourierFeatures(kernel, num_basis_functions)
+state = ff.init_state(kernel_params, num_samples, key)
+f = ff(kernel_params, state, m)
+
+m_ff = jnp.mean(f,axis=0)
+k_ff = jnp.mean(f[..., :, np.newaxis, :, np.newaxis] * f[..., np.newaxis, :, np.newaxis, :], axis=0)
+
+# %%
+i = int(n_points**2 / 2)
+
+fig, axs = plt.subplots(2,2, figsize=(8,8))
+
+axs[0,0].contourf(
+    m[:,0].reshape((n_points, n_points)), 
+    m[:,1].reshape((n_points, n_points)), 
+    m_ff[:,0].reshape((n_points, n_points)),
+    50,
+    vmin=0,
+    vmax=1,
+)
+axs[0,0].set_aspect('equal')
+axs[0,0].set_title("Fourier featuer mean")
+
+axs[0,1].contourf(
+    m[:,0].reshape((n_points, n_points)), 
+    m[:,1].reshape((n_points, n_points)), 
+    jnp.abs(k[i,:,0,0] - k_ff[i,:,0,0]).reshape((n_points, n_points)),
+    20,
+    vmin=0,
+    vmax=1,
+)
+axs[0,1].set_aspect('equal')
+axs[0,1].set_title("Kernel error")
+
+
+
+axs[1,0].contourf(
+    m[:,0].reshape((n_points, n_points)), 
+    m[:,1].reshape((n_points, n_points)), 
+    k_ff[i,:,0,0].reshape((n_points, n_points)),
+    20,
+    vmin=0,
+    vmax=1,
+)
+axs[1,0].set_aspect('equal')
+axs[1,0].set_title("Fourier feature")
+
+cf = axs[1,1].contourf(
+    m[:,0].reshape((n_points, n_points)), 
+    m[:,1].reshape((n_points, n_points)), 
+    k[i,:,0,0].reshape((n_points, n_points)),
+    20,
+    vmin=0,
+    vmax=1,
+)
+axs[1,1].set_aspect('equal')
+axs[1,1].set_title("Computed")
+
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+fig.colorbar(cf, cax=cbar_ax)
+# %%
+s1 = EmbeddedS1(0.5) # produce base manifold
+num_basis_functions = 1000
+num_samples = 2000
+man = EmbeddedProductManifold(s1,s1, num_eigenfunctions=num_basis_functions)
+
+n_points = 25
+m = np.linspace(0, jnp.pi*2, n_points+1, dtype=jnp.float64)[:-1] % (2 * jnp.pi)
+m = jnp.meshgrid(m,m)
+m = jnp.stack([m_.flatten() for m_ in m], axis=-1)
+
+kernel = ScaledKernel(ManifoldProjectionVectorKernel(MaternCompactRiemannianManifoldKernel(0.5,man, num_basis_functions), man))
+kernel_params = kernel.init_params(next(rng))
+sub_kernel_params = kernel_params.sub_kernel_params
+sub_kernel_params = sub_kernel_params._replace(log_length_scale=jnp.log(0.1))
+kernel_params = kernel_params._replace(sub_kernel_params=sub_kernel_params)
+kernel_params = kernel_params._replace(log_amplitude=-jnp.log(kernel.matrix(kernel_params, m,m)[0,0,0,0]))
+k = kernel.matrix(kernel_params, m, m)
+key = next(rng)
+
+ff = FourierFeatures(kernel, num_basis_functions)
+state = ff.init_state(kernel_params, num_samples, key)
+f = ff(kernel_params, state, m)
+
+m_ff = jnp.mean(f,axis=0)
+k_ff = jnp.mean(f[..., :, np.newaxis, :, np.newaxis] * f[..., np.newaxis, :, np.newaxis, :], axis=0)
+
+# %%
+
+scale=20
+
+i = int(n_points**2 / 2)
+
+fig, axs = plt.subplots(2,2, figsize=(8,8))
+
+axs[0,0].contourf(
+    m[:,0].reshape((n_points, n_points)), 
+    m[:,1].reshape((n_points, n_points)), 
+    m_ff[:,0].reshape((n_points, n_points)),
+    50,
+    vmin=0,
+    vmax=1,
+)
+axs[0,0].set_aspect('equal')
+axs[0,0].set_title("Fourier featuer mean")
+
+axs[0,1].contourf(
+    m[:,0].reshape((n_points, n_points)), 
+    m[:,1].reshape((n_points, n_points)), 
+    jnp.abs(k[i,:,0,0] - k_ff[i,:,0,0]).reshape((n_points, n_points)),
+    20,
+    vmin=0,
+    vmax=1,
+)
+axs[0,1].set_aspect('equal')
+axs[0,1].set_title("Kernel error")
+
+
+
+axs[1,0].contourf(
+    m[:,0].reshape((n_points, n_points)), 
+    m[:,1].reshape((n_points, n_points)), 
+    k_ff[i,:,0,0].reshape((n_points, n_points)),
+    20,
+    vmin=0,
+    vmax=1,
+)
+axs[1,0].set_aspect('equal')
+axs[1,0].set_title("Fourier feature")
+
+cf = axs[1,1].contourf(
+    m[:,0].reshape((n_points, n_points)), 
+    m[:,1].reshape((n_points, n_points)), 
+    k[i,:,0,0].reshape((n_points, n_points)),
+    20,
+    vmin=0,
+    vmax=1,
+)
+axs[1,1].set_aspect('equal')
+axs[1,1].set_title("Computed")
+
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+fig.colorbar(cf, cax=cbar_ax)
+# %%
+i=110
+scale=50
+plt.quiver(
+    m[:,0],
+    m[:,1],
+    f[i,:,0],
+    f[i,:,1],
+    color='blue',
+    scale=scale
+)
+plt.gca().set_aspect('equal')
+# %%
