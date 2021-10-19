a52b062769df515ffb9136d9fe6f35b12038bee5
mjhutchinson <hutchinson.michael.john@gmail.com>
2021-10-19 10:21:55 +0100

    rendering

---
+++ blender_scripts/complie_all.sh
@@ -0,0 +1,5 @@
+blender -b -P complie_video.py /home/mjhutchinson/Documents/projects/ExtrinsicGaugeEquivariantVectorGPs/blender/blank_to_wrong/renders 0
+# blender -b -P complie_video.py /home/mjhutchinson/Documents/projects/ExtrinsicGaugeEquivariantVectorGPs/blender/rewrap_sphere/renders 1
+# blender -b -P complie_video.py /home/mjhutchinson/Documents/projects/ExtrinsicGaugeEquivariantVectorGPs/blender/satellite_tracks/renders 0
+# blender -b -P complie_video.py /home/mjhutchinson/Documents/projects/ExtrinsicGaugeEquivariantVectorGPs/blender/unwrap_sphere/renders 0
+blender -b -P complie_video.py /home/mjhutchinson/Documents/projects/ExtrinsicGaugeEquivariantVectorGPs/blender/wrong_to_right/renders 0
--- blender_scripts/do_all.sh
+++ blender_scripts/do_all.sh
@@ -1,4 +1,4 @@
-blender -b -P blank_to_wronng.py
+blender -b -P blank_to_wrong.py
 blender -b -P rewrap_sphere.py
 blender -b -P satellite_tracks.py
 blender -b -P unwrap_sphere.py
